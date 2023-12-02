ea0055b2863bb0a612fe6211c0a1f22cd6e242dd
Shadab <sadabia854318@gmail.com>
2023-12-02 19:23:24 +0530

    updated user

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { REST, Routes, Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 import http from "http";
-
-const server = http
-  .createServer((req, res) => {
+http
+  .createServer((res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("Hello, World!\n");
@@ -13,6 +12,7 @@ const server = http
   });
 
 dotenv.config();
+// const client = new Client({ intents: [GatewayIntentBits.GuildMessages] });
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const commands = [
   {
@@ -34,6 +34,7 @@ client.on("ready", () => {
 });
 
 client.on("interactionCreate", async (interaction) => {
+  console.log(interaction.user,"created");
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "who_are_you") {
@@ -44,6 +45,24 @@ client.on("interactionCreate", async (interaction) => {
   }
   if (interaction.commandName === "how_may_i_help_you") {
     await interaction.reply(
+      // client.on("messageCreate", async (message) => {
+      //   console.log(message,"message");
+      //   // Ignore messages from bots or without prefix
+      //   if (message.author.bot || !message.content.startsWith("?")) return;
+
+      //   // Get the command name and arguments from the message
+      //   const args = message.content.slice(1).trim().split(/ +/);
+      //   const commandName = args.shift().toLowerCase();
+
+      //   // Execute the command based on the name
+      //   if (commandName === "who_are_you") {
+      //     await message.channel.send("I am all in one assistance of ava!");
+      //   }
+      //   if (commandName === "hi_there") {
+      //     await message.channel.send("Hello, how are you!");
+      //   }
+      //   if (commandName === "how_may_i_help_you") {
+      //     await message.channel.send(
       "please raise a ticket for better resolution from our team thank you!"
     );
   }
