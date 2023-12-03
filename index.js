5007d4f712253184345f0fb23362969bdec5c0da
Shadab <sadabia854318@gmail.com>
2023-12-03 08:29:20 +0530

    added messages

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ http
 
 dotenv.config();
 // const client = new Client({ intents: [GatewayIntentBits.GuildMessages] });
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
 const commands = [
   {
     name: "hi_there",
@@ -33,36 +33,39 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("interactionCreate", async (interaction) => {
-  console.log(interaction.user);
-  if (!interaction.isChatInputCommand()) return;
+// client.on("interactionCreate", async (interaction) => {
+//   console.log(interaction.user);
+//   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName === "who_are_you") {
-    await interaction.reply("i am all in one assistance of ava!");
-  }
-  if (interaction.commandName === "hi_there") {
-    await interaction.reply("hello, how are you!");
-  }
-  if (interaction.commandName === "how_may_i_help_you") {
-    await interaction.reply(
-      // client.on("messageCreate", async (message) => {
-      //   console.log(message,"message");
-      //   // Ignore messages from bots or without prefix
-      //   if (message.author.bot || !message.content.startsWith("?")) return;
+//   if (interaction.commandName === "who_are_you") {
+//     await interaction.reply("i am all in one assistance of ava!");
+//   }
+//   if (interaction.commandName === "hi_there") {
+//     await interaction.reply("hello, how are you!");
+//   }
+//   if (interaction.commandName === "how_may_i_help_you") {
+//     await interaction.reply(
+      client.on("messageCreate", async (message) => {
+        console.log(message.content,"message");
+        // Ignore messages from bots or without prefix
+        // if (message.author.bot || !message.content.startsWith("?")) return;
+        if (message.author.bot) return;
 
-      //   // Get the command name and arguments from the message
-      //   const args = message.content.slice(1).trim().split(/ +/);
-      //   const commandName = args.shift().toLowerCase();
+        // Get the command name and arguments from the message
+        // const args = message.content.slice(1).trim().split(/ +/);
+        // const commandName = args.shift().toLowerCase();
 
-      //   // Execute the command based on the name
-      //   if (commandName === "who_are_you") {
-      //     await message.channel.send("I am all in one assistance of ava!");
-      //   }
-      //   if (commandName === "hi_there") {
-      //     await message.channel.send("Hello, how are you!");
-      //   }
-      //   if (commandName === "how_may_i_help_you") {
-      //     await message.channel.send(
+        const commandName = message.content;
+
+        // Execute the command based on the name
+        if (commandName === "who are you") {
+          await message.channel.send("I am all in one assistance of ava!");
+        }
+        if (commandName === "hi there") {
+          await message.channel.send("Hello, how are you!");
+        }
+        if (commandName === "how may i help you") {
+          await message.channel.send(
       "please raise a ticket for better resolution from our team thank you!"
     );
   }
